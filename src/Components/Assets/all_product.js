26c18cd763722989240b238ef096a96c0db07158
Kalpesh Koli <kolikalpesh65@gmail.com>
2024-04-17 12:55:11 +0530

    2nd commit

---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,22 @@
+import p1_img from './shirt1.jpg'
+import p2_img from './shirt2.jpg'
+
+let all_product = [
+    {
+        id:1,
+        name:"Launching a clothing line requires a unique and consistent brand identity that sets itself apart from your competitors.",
+        category:"men",
+        image:p1_img,
+        new_price:50.0,
+        old_price:80.0
+    },
+    {
+        id:2,
+        name:"Launching a clothing line requires a unique and consistent brand identity that sets itself apart from your competitors.",
+        category:"women",
+        image:p2_img,
+        new_price:50.0,
+        old_price:80.0
+    },
+];
+export default all_product;
---
+++ src/Components/Assets/data.js
@@ -0,0 +1,20 @@
+import p1_img from './shirt1.jpg';
+import p2_img from './shirt2.jpg';
+
+let data_product = [
+    {
+        id:1,
+        name:"Launching a clothing line requires a unique and consistent brand identity that sets itself apart from your competitors.",
+        image:p1_img,
+        new_price:60.0,
+        old_price:80.0
+    },
+    {
+        id:2,
+        name:"Launching a clothing line requires a unique and consistent brand identity that sets itself apart from your competitors.",
+        image:p2_img,
+        new_price:70.0,
+        old_price:80.0
+    }
+];
+export default data_product;
